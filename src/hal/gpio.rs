3159732eaa1b71bf0681492ded386cadca322d4a
ruihs <mafioznikkloyn@gmail.com>
2026-07-08 18:14:06 +0300

    adding macro for PartsA and PartsB and split functions. Adding docs to git repo
    adding macro for PartsA and PartsB and split functions

--- src/hal/gpio.rs
+++ src/hal/gpio.rs
@@ -108,106 +108,23 @@ impl<MODE> Pin<MODE> {
     }
 }
 
-pub struct PartsA {
-    pub pa0: Pin<Input>,
-    pub pa1: Pin<Input>,
-    pub pa2: Pin<Input>,
-    pub pa3: Pin<Input>,
-    pub pa4: Pin<Input>,
-    pub pa5: Pin<Input>,
-    pub pa6: Pin<Input>,
-    pub pa7: Pin<Input>,
-    pub pa8: Pin<Input>,
-    pub pa9: Pin<Input>,
-    pub pa10: Pin<Input>,
-    pub pa11: Pin<Input>,
-    pub pa12: Pin<Input>,
-    pub pa13: Pin<Input>,
-    pub pa14: Pin<Input>,
-    pub pa15: Pin<Input>,
+macro_rules! gpio {
+    ($Parts:ident, $split:ident, $Gpio:ty, $port:expr, [ $($name:ident : $num:literal),+ $(,)? ]) => {
+        pub struct $Parts {
+            $( pub $name: Pin<Input>, )+
+        }
+        pub fn $split(_gpio: $Gpio) -> $Parts {
+            $Parts {
+                $( $name: Pin { port: $port, pin: $num, _mode: PhantomData }, )+
+            }
+        }
+    };
 }
 
-pub fn split_gpioa(_gpioa: gd32e230::Gpioa) -> PartsA {
-    PartsA {
-        pa0: Pin {
-            port: Port::A,
-            pin: 0,
-            _mode: PhantomData,
-        },
-        pa1: Pin {
-            port: Port::A,
-            pin: 1,
-            _mode: PhantomData,
-        },
-        pa2: Pin {
-            port: Port::A,
-            pin: 2,
-            _mode: PhantomData,
-        },
-        pa3: Pin {
-            port: Port::A,
-            pin: 3,
-            _mode: PhantomData,
-        },
-        pa4: Pin {
-            port: Port::A,
-            pin: 4,
-            _mode: PhantomData,
-        },
-        pa5: Pin {
-            port: Port::A,
-            pin: 5,
-            _mode: PhantomData,
-        },
-        pa6: Pin {
-            port: Port::A,
-            pin: 6,
-            _mode: PhantomData,
-        },
-        pa7: Pin {
-            port: Port::A,
-            pin: 7,
-            _mode: PhantomData,
-        },
-        pa8: Pin {
-            port: Port::A,
-            pin: 8,
-            _mode: PhantomData,
-        },
-        pa9: Pin {
-            port: Port::A,
-            pin: 9,
-            _mode: PhantomData,
-        },
-        pa10: Pin {
-            port: Port::A,
-            pin: 10,
-            _mode: PhantomData,
-        },
-        pa11: Pin {
-            port: Port::A,
-            pin: 11,
-            _mode: PhantomData,
-        },
-        pa12: Pin {
-            port: Port::A,
-            pin: 12,
-            _mode: PhantomData,
-        },
-        pa13: Pin {
-            port: Port::A,
-            pin: 13,
-            _mode: PhantomData,
-        },
-        pa14: Pin {
-            port: Port::A,
-            pin: 14,
-            _mode: PhantomData,
-        },
-        pa15: Pin {
-            port: Port::A,
-            pin: 15,
-            _mode: PhantomData,
-        },
-    }
-}
+gpio!(PartsA, split_gpioa, gd32e230::Gpioa, Port::A,
+    [pa0:0, pa1:1, pa2:2, pa3:3, pa4:4, pa5:5, pa6:6, pa7:7,
+     pa8:8, pa9:9, pa10:10, pa11:11, pa12:12, pa13:13, pa14:14, pa15:15]);
+
+gpio!(PartsB, split_gpiob, gd32e230::Gpiob, Port::B,
+    [pb0:0, pb1:1, pb2:2, pb3:3, pb4:4, pb5:5, pb6:6, pb7:7,
+     pb8:8, pb9:9, pb10:10, pb11:11, pb12:12, pb13:13, pb14:14, pb15:15]);
